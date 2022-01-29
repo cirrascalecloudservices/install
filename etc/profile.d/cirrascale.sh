e70b267a0e2f6a1c2dc4f9104ccb204d91798c48
rrizun <rrizun@rrizuns-MacBook-Air.local>
2022-01-28 16:44:24 -0800

    /etc/profile.d/cirrascale.sh

---
+++ etc/profile.d/cirrascale.sh
@@ -0,0 +1,3 @@
+# https://docs.nvidia.com/cuda/cuda-quick-start-guide/index.html#ubuntu-x86_64-deb
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
