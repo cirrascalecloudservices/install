d115de7c9c49b5c1e981f590cabc4451d27bf82c
rrizun <rrizun@gmail.com>
2022-02-07 14:02:06 -0800

    Update ubuntu-focal.sh

--- ubuntu-focal.sh
+++ ubuntu-focal.sh
@@ -5,5 +5,5 @@ systemctl set-default multi-user.target
 echo 'cirrascale ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/cirrascale
 
 # install nvidia cuda
-sh -c "$(curl -sL https://raw.githubusercontent.com/cirrascalecloudservices/install/main/install-nvidia-cuda-11.5-495.sh)"
+#sh -c "$(curl -sL https://raw.githubusercontent.com/cirrascalecloudservices/install/main/install-nvidia-cuda-11.5-495.sh)"
 (cd /etc/profile.d && wget https://raw.githubusercontent.com/cirrascalecloudservices/install/main/etc/profile.d/cirrascale.sh)
