bf5506df11979f20ad16cc1a03d9755ae8ff4415
rrizun <rrizun@rrizuns-MacBook-Air.local>
2022-03-23 18:17:49 -0700

    cuda, cuda-driver, cudnn

--- apt-install-cuda.sh
+++ apt-install-cuda.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-# https://developer.nvidia.com/cuda-toolkit-archive
-
 systemctl set-default multi-user.target
+
+# https://developer.nvidia.com/cuda-toolkit-archive
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -10,7 +10,22 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update
 
-if [ $VERSION ]; then
-	apt-get -y install cuda=$VERSION
-	apt-mark hold cuda
+if [ $CUDA ]; then
+	apt-get -y install cuda-toolkit-$CUDA
 fi
+
+if [ $CUDA_DRIVER ]; then
+	apt-get -y install cuda-drivers-$CUDA_DRIVER
+fi
+
+###TODO fabricmanager here
+
+if [ $CUDNN ]; then
+	apt-get -y install libcudnn8=$CUDNN && apt-mark hold libcudnn8
+fi
+
+# https://docs.nvidia.com/cuda/cuda-quick-start-guide/index.html#debian-x86_64-deb
+cat > /etc/profile.d/cirrascale-cuda.sh << 'EOF'
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+EOF
