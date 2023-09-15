5bc9cfaf83166160a46415c92b09498115701da5
willhanau-cirrascale <39277475+willhanau-cirrascale@users.noreply.github.com>
2023-09-15 04:56:48 -0700

    Update install-QAI100-prereqs.sh

--- install-QAI100-prereqs.sh
+++ install-QAI100-prereqs.sh
@@ -7,6 +7,7 @@ apt-get install dkms -y
 apt-get install linux-headers-$(uname -r) -y
 apt-get update && apt-get install -y software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
+add-apt-repository ppa:deadsnakes/ppa -y
 apt-get update && apt-get install -y build-essential git vim libpci-dev libudev-dev python3-pip python3-setuptools python3-wheel python3.8 python3.8-dev python3.8-venv
 apt-get update && apt-get install -y unzip wget ca-certificates pciutils libglib2.0-dev libssl-dev snap snapd libgl1-mesa-glx pkg-config clang-format libpng-dev
 apt-get install libstdc++6 -y
