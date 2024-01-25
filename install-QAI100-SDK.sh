27efe2c8b6c3f224451d08eb13635626fecad779
willhanau-cirrascale <39277475+willhanau-cirrascale@users.noreply.github.com>
2024-01-25 13:29:29 -0800

    Update install-QAI100-SDK.sh

--- install-QAI100-SDK.sh
+++ install-QAI100-SDK.sh
@@ -8,22 +8,17 @@ APPS_SDK_ZIP=qaic-apps-${QAIC_VERSION}.zip
 APPS_SDK=qaic-apps-${QAIC_VERSION}
 
 # install platform SDK
-cd ${DOWNLOADS_PATH}
 unzip ${DOWNLOADS_PATH}/${PLATFORM_SDK_ZIP}
-cd ${DOWNLOADS_PATH}/${PLATFORM_SDK}/x86_64/deb/
-./install.sh --auto_upgrade_sbl --ecc enable
+cd ${DOWNLOADS_PATH}/${PLATFORM_SDK}/x86_64/deb/ && ./install.sh --auto_upgrade_sbl --ecc enable
 
 # install apps SDK
-cd ${DOWNLOADS_PATH}
 unzip ${DOWNLOADS_PATH}/${APPS_SDK_ZIP}
-cd ${DOWNLOADS_PATH}/${APPS_SDK}
-./install.sh
+cd ${DOWNLOADS_PATH}/${APPS_SDK} && ./install.sh
 chmod a+x /opt/qti-aic/dev/hexagon_tools/bin/*
 chmod a+x /opt/qti-aic/exec/*
 
 # install qaic-pytools
-cd ${DOWNLOADS_PATH}/${APPS_SDK}/tools/qaic-pytools/
-./install.sh
+cd ${DOWNLOADS_PATH}/${APPS_SDK}/tools/qaic-pytools/ && ./install.sh
 
 # give user permissions for installed SDKs
 chown -R ${TARGET_USER}:${TARGET_USER} /opt/qti-aic/
