3bced41f680c82f1ec92d0ec696b704d4d42271b
Anh-Nguyen2k <138734193+Anh-Nguyen2k@users.noreply.github.com>
2024-09-23 16:00:42 -0700

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/bash -eux
+set -o pipefail
+shopt -s inherit_errexit || true
 sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=15/' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT_STYLE=hidden/# GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
