4dd0190435f3c15f77a092a4aaa0793b9cabf35a
Anh-Nguyen2k <138734193+Anh-Nguyen2k@users.noreply.github.com>
2024-10-28 09:59:02 -0700

    Update after-install.sh
    made sed commands more flexible by clearing all instances of sepcified GRUB commands with default options or not

--- after-install.sh
+++ after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 set -o pipefail
 shopt -s inherit_errexit || true
-sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=15/' /etc/default/grub
-sed -i 's/GRUB_TIMEOUT_STYLE=hidden/# GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=15/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT_STYLE=.*/# GRUB_TIMEOUT_STYLE=hidden/g' /etc/default/grub
