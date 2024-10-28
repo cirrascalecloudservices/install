17f8d9b00d95f0e4e78de52a83df8d30c6225be7
Anh-Nguyen2k <138734193+Anh-Nguyen2k@users.noreply.github.com>
2024-10-28 10:30:24 -0700

    Update after-install.sh
    added update-grub command to apply modifications

--- after-install.sh
+++ after-install.sh
@@ -3,3 +3,4 @@ set -o pipefail
 shopt -s inherit_errexit || true
 sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=15/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT_STYLE=.*/# GRUB_TIMEOUT_STYLE=hidden/g' /etc/default/grub
+update-grub
