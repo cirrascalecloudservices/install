fe5971ab7909f0244491251b9b6335b78f17b970
willhanau-cirrascale <39277475+willhanau-cirrascale@users.noreply.github.com>
2024-12-06 04:11:02 -0800

    Update install-QAI100-prereqs.sh

--- install-QAI100-prereqs.sh
+++ install-QAI100-prereqs.sh
@@ -8,16 +8,14 @@ apt-get install linux-headers-$(uname -r) -y
 apt-get update && apt-get install -y software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 add-apt-repository ppa:deadsnakes/ppa -y
-apt-get update && apt-get install -y build-essential git vim libpci-dev libudev-dev python3-pip python3-setuptools python3-wheel python3.8 python3.8-dev python3.8-venv
-apt-get update && apt-get install -y unzip wget ca-certificates pciutils libglib2.0-dev libssl-dev snap snapd libgl1-mesa-glx pkg-config clang-format libpng-dev
+apt-get update && apt-get install -y build-essential git less vim-tiny nano libpci-dev libudev-dev libatomic1 python3-pip python3-setuptools python3-wheel python3.8 python3.8-dev python3.8-venv
+apt-get update && apt-get install -y unzip zip wget ca-certificates sudo pciutils libglib2.0-dev libssl-dev snap snapd openssh-server pkg-config clang-format libpng-dev gpg
 apt-get install libstdc++6 -y
-apt-get install libncurses5 -y
+apt-get install libncurses5-dev -y
 
-pip3 install --upgrade pip
-pip3 install wheel numpy opencv-python onnx pyudev
 python3.8 -m pip install --upgrade pip
-python3.8 -m pip install wheel numpy opencv-python onnx pyudev
+python3.8 -m pip install wheel numpy opencv-python onnx pyudev networkx
 
 # docker support
-apt install docker.io -y
+curl https://get.docker.com | bash
 usermod -aG docker $TARGET_USER
