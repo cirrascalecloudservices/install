2f5c31a59ebed38a256bca6f0205a9c5fa05404d
willhanau-cirrascale <39277475+willhanau-cirrascale@users.noreply.github.com>
2025-02-25 06:23:55 -0800

    Add python3.10-venv package to support qaic 1.19

--- install-QAI100-prereqs.sh
+++ install-QAI100-prereqs.sh
@@ -8,7 +8,7 @@ apt-get install linux-headers-$(uname -r) -y
 apt-get update && apt-get install -y software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 add-apt-repository ppa:deadsnakes/ppa -y
-apt-get update && apt-get install -y build-essential git less vim-tiny nano libpci-dev libudev-dev libatomic1 python3-pip python3-setuptools python3-wheel python3.8 python3.8-dev python3.8-venv
+apt-get update && apt-get install -y build-essential git less vim-tiny nano libpci-dev libudev-dev libatomic1 python3-pip python3-setuptools python3-wheel python3.8 python3.8-dev python3.8-venv python3.10-venv
 apt-get update && apt-get install -y unzip zip wget ca-certificates sudo pciutils libglib2.0-dev libssl-dev snap snapd openssh-server pkg-config clang-format libpng-dev gpg
 apt-get install libstdc++6 -y
 apt-get install libncurses5-dev -y
