3fb9e6aa7e983cd2bd63e92e70ac16688917cb39
willhanau-cirrascale <will.hanau@cirrascale.com>
2025-08-04 14:19:16 -0700

    added pcie IDs to check for nvlink5 and nvswitch devices. minor refactors. use nvidia-driver-assistant to check if GPU is open driver compatible.

--- install-cuda.sh
+++ install-cuda.sh
@@ -2,17 +2,21 @@
 # Example: CUDA= CUDA_DRIVER= CUDNN= NCCL= USE_OPEN_DRIVER= sh -c "$(curl -s https://raw.githubusercontent.com/cirrascalecloudservices/install/Test/install-cuda.sh)"
 # sudo sh -c "$(curl -s https://raw.githubusercontent.com/cirrascalecloudservices/install/main/install-cuda.sh)"
 
+# A100-HGX-nvswitch,H100/200-HGX-nvswitch,B200-HGX-SXM2(due to arch changes, nvswitch does not have its own pcie device)
+NVSWITCH_PCIE_IDS="10de:1af1|10de:22a3|10de:2901"
+# B200-HGX-SXM2
+NVL5_GPU_PCIE_IDS="10de:2901"
 . /etc/os-release
 
 systemctl set-default multi-user.target
 
 # https://developer.nvidia.com/cuda-toolkit-archive
-arch=x86_64
+arch=$(uname -m)
 distro=ubuntu$(echo $VERSION_ID | tr -d .)
 
 LATEST_CUDA_DRIVER=$(curl -s https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ | grep -oP 'nvidia-driver-\K[0-9]+' | sort -n | tail -n 1)
-NVSWITCH_FOUND=$(lspci | grep -i "Bridge: NVIDIA")
-NVL5_FOUND=$(lspci | grep -i "NVIDIA Corporation Device 2901")
+NVSWITCH_FOUND=$(lspci -nn | grep -E "($NVSWITCH_PCIE_IDS)")
+NVL5_FOUND=$(lspci -nn | grep -E "($NVL5_GPU_PCIE_IDS)")
 
 # https://forums.developer.nvidia.com/t/notice-cuda-linux-repository-key-rotation/212772
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#network-repo-installation-for-ubuntu
@@ -22,47 +26,55 @@ dpkg -i $(basename $(curl -s -w "%{url_effective}" https://developer.download.nv
 apt-get install -y linux-headers-$(uname -r)
 
 # install cuda library
-if [ "$CUDA" ]; then
+if [ -n "$CUDA" ]; then
 	apt-get install -y cuda-toolkit-$CUDA -y && apt-mark hold cuda-toolkit-$CUDA
 else
 	apt-get install -y cuda-toolkit -y && apt-mark hold cuda-toolkit
 fi
 
-# install nvidia driver
-if [ !"$CUDA_DRIVER" ]; then
+# install latest cuda driver if one not set
+if [ -z "$CUDA_DRIVER" ]; then
 	CUDA_DRIVER=$LATEST_CUDA_DRIVER
 fi
 
-if [ "$INSTALL_OPEN_DRIVER" ]; then
+# setting $INSTALL_OPEN_DRIVER will force installing the open driver
+# if $INSTALL_OPEN_DRIVER not set, check if GPU is open driver compatible
+if [ -z "$INSTALL_OPEN_DRIVER" ]; then
+	apt install nvidia-driver-assistant -y
+	$INSTALL_OPEN_DRIVER=$(nvidia-driver-assistant | grep "nvidia-open")
+fi
+
+# install nvidia open driver, else install nvidia proprietary driver
+if [ -n "$INSTALL_OPEN_DRIVER" ]; then
 	apt install -y nvidia-driver-$CUDA_DRIVER-open nvidia-modprobe && apt-mark hold nvidia-driver-$CUDA_DRIVER-open
-	if [ "$NVSWITCH_FOUND" ]; then
+	if [ -n "$NVSWITCH_FOUND" ]; then
 			apt-get install -y nvidia-fabricmanager-$CUDA_DRIVER -y && apt-mark hold nvidia-fabricmanager-$CUDA_DRIVER
-			systemctl enable nvidia-fabricmanager.service --now
+			systemctl enable nvidia-fabricmanager.service
 	fi
 else
-	if [ "$NVSWITCH_FOUND" ]; then
+	if [ -n "$NVSWITCH_FOUND" ]; then
 		apt-get install cuda-drivers-fabricmanager-$CUDA_DRIVER -y && apt-mark hold cuda-drivers-fabricmanager-$CUDA_DRIVER
-		systemctl enable nvidia-fabricmanager.service --now
+		systemctl enable nvidia-fabricmanager.service
 	else
 		apt-get install cuda-drivers-$CUDA_DRIVER -y && apt-mark hold cuda-drivers-$CUDA_DRIVER
 	fi
 fi
 
 # install nvlsm for Gen5 nvlink systems
-if [ "$NVL5_FOUND" ]; then
+if [ -n "$NVL5_FOUND" ]; then
 	apt-get install -y nvlsm -y && apt-mark hold nvlsm
 fi
 
 # install cudnn
-if [ "$CUDNN" ]; then
-	apt-get install -y libcudnn9-cuda-12=$CUDNN libcudnn9-dev-cuda-12=$CUDNN -y && apt-mark hold libcudnn9-cuda-12 libcudnn9-dev-cuda-12
+if [ -n "$CUDNN" ]; then
+	apt-get install -y libcudnn9-cuda-12=$CUDNN libcudnn9-dev-cuda-12=$CUDNN -y && apt-mark hold libcudnn9-cuda-12=$CUDNN libcudnn9-dev-cuda-12=$CUDNN
 else
 	apt-get install -y libcudnn9-cuda-12 libcudnn9-dev-cuda-12 -y && apt-mark hold libcudnn9-cuda-12 libcudnn9-dev-cuda-12
 fi
 
 # install nccl
-if [ "$NCCL" ]; then
-	apt-get install -y libnccl2=$NCCL libnccl-dev=$NCCL && apt-mark hold libnccl2 libnccl-dev
+if [ -n "$NCCL" ]; then
+	apt-get install -y libnccl2=$NCCL libnccl-dev=$NCCL && apt-mark hold libnccl2=$NCCL libnccl-dev=$NCCL
 else
 	apt-get install -y libnccl2 libnccl-dev && apt-mark hold libnccl2 libnccl-dev
 fi
